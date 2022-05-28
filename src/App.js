8a05aafd85ed2e87e778ef79e3379eb2dcb9c26b
arunrajbhar123 <arunrajbhar1998@gmail.com>
2022-05-28 06:04:15 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SimpleSidebar from './components/Sidebar.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleSidebar/>
+    </div>
+  );
+}
+
+export default App;
